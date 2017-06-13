0165f83ba9216a41ee9c1c7fce66f6b05119d36d
rayGreycloud <rayGreycloud@gmail.com>
2017-06-13 12:13:22 -0700

    Initial commit of starter code

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp        = require('gulp');
+var clean       = require('gulp-clean');
+var concat      = require('gulp-concat');
+var sass        = require('gulp-sass');
+var uglify      = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var del         = require('del');
+var browserSync = require('browser-sync');
+
+var buildDir    = 'bin/';
+var javaScriptFiles     = ['js/general/app.js'];
+
+
+gulp.task('default', function ()
+{
+    return gulp.src(javaScriptFiles)
+        .pipe(concat('js/d3jsTutorial.js'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('scripts', function ()
+{
+    console.log('run scripts!');
+    return gulp.src(javaScriptFiles)
+        .pipe(concat('js/d3jsTutorial.js'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass', function ()
+{
+    gulp.src('css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css/'));
+});
+
+/** run watch **/
+gulp.task('watch', function ()
+{
+    gulp.watch('js/**/*.js', ['scripts']);
+    //gulp.watch('css/**/*.scss', ['sass']);
+});
+
+gulp.task('startBrowser', function ()
+{
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    })
+});
+
+
