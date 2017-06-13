acb6811a3f4919977ed1655acb4237fd65041b52
rayGreycloud <rayGreycloud@gmail.com>
2017-06-13 14:07:28 -0700

    Add svg examples

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var del         = require('del');
 var browserSync = require('browser-sync');
 
 var buildDir    = 'bin/';
-var javaScriptFiles     = ['js/general/app.js'];
+var javaScriptFiles     = ['js/general/*.js'];
 
 
 gulp.task('default', function ()
@@ -48,5 +48,3 @@ gulp.task('startBrowser', function ()
         }
     })
 });
-
-
--- js/general/svgExample.js
+++ js/general/svgExample.js
@@ -1,12 +1,3 @@
-/** this is how we start d3js with javascript **/
-function start ()
-{
-    d3.select("body")
-        .append("p")
-        .text("Load text with d3.js! today");
-
-    console.log(d3);
-}
 function svgExample() {
   var canvas = d3.select("body")
     .append("svg")
