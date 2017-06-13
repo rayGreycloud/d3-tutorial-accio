2999c5a16d0ec49245d11ad97627f17828bf1ba1
rayGreycloud <rayGreycloud@gmail.com>
2017-06-13 16:39:33 -0700

    Using groups and axes

--- js/d3jsTutorial.js
+++ js/d3jsTutorial.js
@@ -7,6 +7,46 @@ function start ()
 
     console.log(d3);
 }
+function axisGroups() {
+
+  const graphData = [10, 800, 100, 1200],
+    w = 800,
+    h = 800;
+
+  let graphScaling = d3.scaleLinear()
+    .domain([0,1200])
+    .range([0,w]);
+
+  let axis = d3.axisBottom()
+    .ticks(5)
+    .scale(graphScaling);
+
+  const canvas = d3.select(".graphContainer")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("g")
+    .attr("transform", "translate(0,20)");
+
+  let graphBars = canvas.selectAll("rect")
+    .data(graphData)
+    .enter()
+    .append("rect")
+    .attr("fill", "darksalmon")
+    .attr("width", function (d) {
+      return graphScaling(d);
+    })
+    .attr("height", 20)
+    .attr("y", function (d,i) {
+      return i * 50;
+    });
+
+  canvas.append("g")
+    .attr("transform", "translate(0, 200)")
+    .call(axis);
+
+}
+
 function scaling() {
   const graphData = [10, 800, 100, 1200],
     w = 800,
