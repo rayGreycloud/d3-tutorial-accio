dd8c87c9ea11da6b868186dfbee826c13e7994c7
rayGreycloud <rayGreycloud@gmail.com>
2017-06-14 11:55:01 -0700

    Using bargraphs

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var browserSync = require('browser-sync');
 var buildDir    = 'bin/';
 var javaScriptFiles     = ['js/general/*.js'];
 var graphJSFiles     = ['js/graph/*.js'];
+var barGraphJSFiles     = ['js/bargraph/*.js'];
 
 
 gulp.task('default', function ()
@@ -17,6 +18,12 @@ gulp.task('default', function ()
     return gulp.src(javaScriptFiles)
         .pipe(concat('js/d3jsTutorial.js'))
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('bargraph', function () {
+  return gulp.src(barGraphJSFiles)
+    .pipe(concat('js/d3jsBarGraph.js'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('graph', function () {
@@ -45,6 +52,7 @@ gulp.task('watch', function ()
 {
   gulp.watch('js/general/*.js', ['scripts']);
   gulp.watch('js/graph/*.js', ['graph']);
+  gulp.watch('js/bargraph/*.js', ['bargraph']);
     // gulp.watch('js/**/*.js', ['scripts']);
     //gulp.watch('css/**/*.scss', ['sass']);
 });
---
+++ js/d3jsBarGraph.js
@@ -0,0 +1,70 @@
+function barGraph() {
+  // set variables
+  const margin = {top: 20, right: 20, bottom: 100, left: 60};
+  const width = 800 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
+  const x = d3.scaleBand().rangeRound([0,width]).paddingInner(0.5);
+  const y = d3.scaleLinear().range([height,0]);
+
+  // draw axes
+  const xAxis = d3.axisBottom()
+    .scale(x)
+    .ticks(6);
+
+  const yAxis = d3.axisLeft()
+    .scale(y)
+    .ticks(5);
+
+  // create svg element
+  const svg = d3.select("#barGraph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // get data
+  d3.json("data/myData.json", function (data) {
+    x.domain(data.map(function (d) {
+      return d.name;
+    }));
+
+    y.domain([0, d3.max(data, function (d) {
+      return d.rank;
+    })]);
+
+    // labels
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-0.5em")
+      .attr("dy", "-0.55em")
+      .attr("y", 30)
+      .attr("transform", "rotate(-45)");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 5)
+      .attr("dy", "0.8em")
+      .style("text-anchor", "end")
+      .text("Member Rank");
+
+    svg.selectAll("bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("fill", "darksalmon")
+      .attr("x", function (d) { return x(d.name); })
+      .attr("width", x.bandwidth())
+      .attr("y", function (d) { return y(d.rank); })
+      .attr("height", function (d) {
+        return height - y(d.rank);
+      });
+  })
+}
