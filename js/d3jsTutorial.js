19fea6be6e90b0c5e5c2f23b2f571b2ad4dbef28
rayGreycloud <rayGreycloud@gmail.com>
2017-06-13 18:16:27 -0700

    Using JSON data

--- js/d3jsTutorial.js
+++ js/d3jsTutorial.js
@@ -73,6 +73,44 @@ function enterExamples() {
     .attr("stroke", "darksalmon");
 }
 
+function importData() {
+
+  d3.json("data/myData.json", function (data) {
+
+    const canvas = d3.select(".importDataContainer")
+      .append("svg")
+      .attr("width", 1000)
+      .attr("height", 700);
+
+    canvas.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("width", function (d) {
+        return d.rank * 10;
+      })
+      .attr("height", 50)
+      .attr("y", function (d, i) {
+        return i * 80
+      })
+      .attr("fill", "darkslateblue");
+
+    canvas.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("fill", "#ffffff")
+      .attr("y", function (d, i) {
+        return i * 80 + 30;
+      })
+      .attr("x", 5)
+      .text(function (d) {
+        return `${d.name} rank: ${d.rank}`;
+      })
+  })
+
+}
+
 function scaling() {
   const graphData = [10, 800, 100, 1200],
     w = 800,
